2e2e6a141302c6bd740d75293a5bbae7e0f2ef91
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2022-01-18 23:04:05 +0100

    other changes provided

---
+++ src/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+html {
+  box-sizing: border-box;
+}
+
+*, ::after, ::before {
+  box-sizing: inherit;
+}
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  background: ${({ theme }) => theme.colors.lilyWhite};
+}
+
+button {
+  cursor: pointer;
+}
+
+`;
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+    colors: {
+        lilyWhite: "#E7F8FF",
+        teal: "#008080",
+        white: "#FFFFFF"
+    },
+
+    breakpoint: {
+        mobileMax: 767,
+    },
+}
